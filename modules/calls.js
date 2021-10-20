7073aac85b48d7956357e97315ad98a90887c700
Mateusz Pstrucha <pstrucha.mateusz@gmail.com>
2021-10-21 01:31:46 +0200

    refactor: move track resolving to player

--- modules/calls.js
+++ modules/calls.js
@@ -1,15 +1,19 @@
+import chalk from 'chalk'
 import dotenv from 'dotenv'
 import got from 'got'
+import spotifyToYt from 'spotify-to-yt'
 
 dotenv.config()
 
 const { DOMAIN, SECRET } = process.env,
-	headers = { authorization: 'Bearer ' + SECRET }
+	headers = { authorization: 'Bearer ' + SECRET },
+	verbose = Number(process.env.VERBOSE)
 
 function updateStatus(tid, progress, duration, paused) {
 	return new Promise((resolve, reject) => {
+		verbose > 1 && console.info(`PUT status`, tid, progress, duration, paused)
 		got
-			.put(`${DOMAIN}/player/set/status`, {
+			.put(`https://${DOMAIN}/api/player/set/status`, {
 				headers,
 				json: {
 					tid,
@@ -26,8 +30,9 @@ function updateStatus(tid, progress, duration, paused) {
 
 function updateNext(tid) {
 	return new Promise((resolve, reject) => {
+		verbose > 1 && console.info(`PUT next`, tid)
 		got
-			.put(`${DOMAIN}/player/set/next`, {
+			.put(`https://${DOMAIN}/api/player/set/next`, {
 				headers,
 				json: {
 					tid,
@@ -41,13 +46,22 @@ function updateNext(tid) {
 
 function getTop() {
 	return new Promise((resolve, reject) => {
+		verbose > 1 && console.info(`GET top`)
 		got
-			.get(`${DOMAIN}/player/get/top`, {
+			.get(`https://${DOMAIN}/api/player/get/top`, {
 				headers,
 			})
 			.json()
 			.then((top) => {
-				resolve(top)
+				console.info(`Resolving ${chalk.cyan(top.length)} tracks`)
+				Promise.all(
+					top.map((tid) => spotifyToYt.trackGet(`spotify:track:${tid}`))
+				).then((yts) => {
+					top.map((tid, i) => {
+						yts[i].tid = tid
+					})
+					resolve(yts)
+				})
 			})
 			.catch((err) => reject(err))
 	})
